b3b0ddb149ebfa02b4cb28b32671326565c769a4
EmmhaBoffa <106492795+EmmhaBoffa@users.noreply.github.com>
2023-02-05 18:32:40 -0300

    Update snake.js

--- snake.js
+++ snake.js
@@ -145,7 +145,7 @@ const moverSerpiente = () => {
         aumentarPuntuacion();
         gameBoard.removeChild(food);
         const comer = new Audio("sounds/eat.mp3");
-         comer.volume = 0.4;
+         comer.volume = 0.2;
         comer.play();
         let snakeUnit = document.createElement("div");
         snakeUnit.classList.add("snake-unit");
